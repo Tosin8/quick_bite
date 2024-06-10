e695666848130477e0dee7509260cb7adc253b06
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-10 21:21:10 +0100

    Add Button

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quick_bite/components/form/app_textfield.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,10 +9,14 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final TextEditingController emailController = TextEditingController(); 
+  final TextEditingController passwordController = TextEditingController(); 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
+      //backgroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Colors.grey[300],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -30,9 +35,24 @@ class _LoginScreenState extends State<LoginScreen> {
               color: Theme.of(context).colorScheme.inverseSurface, 
             ),), 
 
+const SizedBox(height: 25,), 
             // email textfield
-            TextField(), 
+            AppTextfield(
+              controller: emailController, 
+              hintText: 'Email', 
+              obscureText: false,
+            ), 
             
+            const SizedBox(height: 10,), 
+              AppTextfield(
+              controller: passwordController, 
+              hintText: 'Password', 
+              obscureText: true,
+            ),
+
+            SizedBox(height: 20,), 
+
+            // Sign In Button. 
           ],
         )
       ),
