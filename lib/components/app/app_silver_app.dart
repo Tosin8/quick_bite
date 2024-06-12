e982ee4c67009498cdc18433871475733a29285b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-12 05:01:32 +0100

    Update app_silver_app.dart

--- lib/components/app/app_silver_app.dart
+++ lib/components/app/app_silver_app.dart
@@ -1,10 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class AppSilverApp extends StatelessWidget {
-  const AppSilverApp({super.key});
+  final Widget child; 
+  final Widget title; 
+  const AppSilverApp({
+    super.key,
+    required this.child,
+    required this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SliverAppBar(
+      backgroundColor: Colors.grey[300], 
+      title: title, 
+      flexibleSpace: FlexibleSpaceBar(
+        background: title,
+      ),
+    );
   }
 }
