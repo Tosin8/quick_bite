dadb1b6593e10790e20a04ce2464fcad9b94002b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-12 06:54:37 +0100

    Create app_tab_bar.dart

---
+++ lib/components/app/app_tab_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
