2f19ed9d15bfc699f8b664be4a41e4fb0c9c6b84
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-12 07:15:37 +0100

    Update food.dart

--- lib/model/food.dart
+++ lib/model/food.dart
@@ -3,7 +3,22 @@ class Food {
   final String description; 
   final String image;
   final double price; 
-  final String category;
+  // final String category;
 
-  Food({required this.name, required this.description, required this.image, required this.price, required this.category});
+  Food({required this.name, required this.description, required this.image, required this.price, 
+  //required this.category
+});
+}
+
+// Enum: a short form of enumerated type , a set of named constants where all possible values are declared. 
+
+// food categories
+
+enum FoodCategory{
+  burgers, 
+  salads, 
+  pizza,
+  pasta, 
+  dessert, 
+  drinks, 
 }
