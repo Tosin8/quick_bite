51f5827db7dbdc5e490efa946486cadea69dfe42
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-15 00:39:17 +0100

    Update payment_page.dart

--- lib/screens/payment_page.dart
+++ lib/screens/payment_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class PaymentPage extends StatelessWidget {
+class PaymentPage extends StatefulWidget {
   const PaymentPage({super.key});
 
+  @override
+  State<PaymentPage> createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
   @override
   Widget build(BuildContext context) {
     return Container();
