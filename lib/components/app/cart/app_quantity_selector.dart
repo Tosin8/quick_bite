bf9a7959309e8f1f29217de4a43e4cca9733a6e9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-14 22:35:39 +0100

    Update app_quantity_selector.dart

--- lib/components/app/cart/app_quantity_selector.dart
+++ lib/components/app/cart/app_quantity_selector.dart
@@ -1,10 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import 'package:quick_bite/model/food.dart';
+
 class AppQuantitySelector extends StatelessWidget {
-  const AppQuantitySelector({super.key});
+  final int quantity; 
+  final Food food; 
+  final VoidCallback onIncrement; 
+  final VoidCallback onDecrement; 
+
+  const AppQuantitySelector({
+    super.key,
+    required this.quantity,
+    required this.food,
+    required this.onIncrement,
+    required this.onDecrement,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all
+        (color: Colors.white), 
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
   }
 }
