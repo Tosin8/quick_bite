efdb05cab03c8e2fb3b67b4178d002b4cbe32053
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-15 03:01:35 +0100

    Update auth_gate.dart

--- lib/services/auth/auth_gate.dart
+++ lib/services/auth/auth_gate.dart
@@ -1,12 +1,30 @@
 // to check if the current user is logged in or not. 
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_bite/screens/home.dart';
+import 'package:quick_bite/services/auth/login_or_register.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot) {
+        // user is logged in . 
+
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        }
+
+        // user is not logged in.
+        else {
+          return const LoginOrRegister();
+        }
+        })
+    );
   }
 }
