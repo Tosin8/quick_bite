b1ce44181580e53f6581f68831409306655ceef0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-15 02:53:59 +0100

    Update auth_services.dart

---
+++ lib/services/auth/auth_services.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+  // get instance of firebase auth
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // get current user
+  User? getCurrentUser(){
+    return _firebaseAuth.currentUser; 
+  }
+
+  // sign in 
+  Future<UserCredential> signInWithEmailPassword(String email, password) async {
+
+    try {
+
+      UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      return userCredential;
+    } on FirebaseAuthException catch (e) {  
+      throw Exception(e.code); 
+    }
+  }
+
+  // sign up
+
+  // sign out
+}
