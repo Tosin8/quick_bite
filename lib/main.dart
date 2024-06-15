7ca288b862b788a78a61795a44d22e5ab7d72b99
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-15 02:45:18 +0100

    Initializing firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quick_bite/firebase_options.dart';
 import 'package:quick_bite/model/restaurant.dart';
 import 'package:quick_bite/screens/splash.dart';
 
 
 import 'themes/theme_provider.dart';
 
-void main(){
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
 
--- lib/screens/delivery_progress_page.dart
+++ lib/screens/delivery_progress_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:quick_bite/screens/app_receipt.dart';
 import 'package:quick_bite/screens/cart_page.dart';
 
@@ -22,7 +23,76 @@ class _DeliveryProgressPageState extends State<DeliveryProgressPage> {
       ),
 
       bottomNavigationBar: _buildBottomNavBar(context),
-      body: AppReceipt(), 
+      body: const AppReceipt(), 
     );
+  }
+
+  // Custom Botom Nav Bar - Message / Call Delivery Driver. 
+  Widget _buildBottomNavBar(BuildContext context){
+    return Container(
+      height: 100,
+      decoration: const BoxDecoration(
+        color: Colors.white, 
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40), 
+          topRight: Radius.circular(40), 
+        )
+      ),
+      padding: const EdgeInsets.all(12),
+      child: Row(
+          children: [
+        
+            // profile pic of the driver
+            Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          shape: BoxShape.circle, 
+        ),
+        child: IconButton(
+          onPressed: (){},
+         icon: const Icon(Icons.person)),
+            ), 
+        
+            const SizedBox(width: 20,), 
+        
+            // driver details
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 14,), 
+                Text('James Billy', style: TextStyle(color: Colors.black, fontSize: 16),),
+                Text('Driver', style: TextStyle(color: Colors.grey, fontSize: 14),),
+              ],
+            ), 
+
+            // Buttons. 
+            Spacer(), 
+            Row(
+            
+              children: [
+                // Message Button
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    shape: BoxShape.circle, 
+                  ),
+                  child: IconButton(onPressed: (){}, 
+                  icon: Icon(Icons.message, size: 24,),),
+                ), 
+SizedBox(width: 10,), 
+                 Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    shape: BoxShape.circle, 
+                  ),
+                  child: IconButton(onPressed: (){}, 
+                  icon: Icon(Icons.call, color: Colors.green, size: 24,),),
+                ),
+              ],
+            )
+          ],
+        ),
+      
+    ); 
   }
 }
