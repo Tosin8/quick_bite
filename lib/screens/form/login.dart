6ef3023029e4d151e2e0115046c99a9d6365c022
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-20 21:47:02 +0100

    done with login

--- lib/screens/form/login.dart
+++ lib/screens/form/login.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quick_bite/components/form/app_button.dart';
 
 import '../../services/auth/auth_services.dart';
+import 'signup.dart';
 // Replace with the actual import
 
 class LoginScreen extends StatefulWidget {
@@ -43,100 +45,154 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
-      body: Center(
-     
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 80, height: 80, 
-                child: Image.asset('assets/icons/logo.png'),
-              ), 
-               const SizedBox(height: 20,), 
-              Text('Quick Bite', 
-              style: TextStyle(
-                fontSize: 16, 
-                color: Theme.of(context).colorScheme.inverseSurface, 
-              ),), 
-              const SizedBox(height: 4,), 
-              Text('Welcome Back', 
-              style: TextStyle(
-                fontSize: 16, 
-                color: Theme.of(context).colorScheme.inverseSurface, 
-              ),), 
-          
-          const SizedBox(height: 25,), 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    errorBorder: const OutlineInputBorder(
-            //borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), 
-            borderSide: BorderSide(color:  Colors.black), 
-          ), 
-                    enabledBorder: const OutlineInputBorder(
-            //borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), 
-            borderSide: BorderSide(color:  Colors.black), 
-          ), 
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), 
-          ), 
-          hintText: 'Email', 
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
        
-          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),  
-                    labelText: 'Email'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-
-              // Password. 
-              
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isPasswordVisible = !_isPasswordVisible;
-                      });
+        body: Center(
+       
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 80, height: 80, 
+                  child: Image.asset('assets/icons/logo.png'),
+                ), 
+                 const SizedBox(height: 20,), 
+                Text('Quick Bite', 
+                style: TextStyle(
+                  fontSize: 16, 
+                  color: Theme.of(context).colorScheme.inverseSurface, 
+                ),), 
+                const SizedBox(height: 4,), 
+                Text('Welcome Back', 
+                style: TextStyle(
+                  fontSize: 16, 
+                  color: Theme.of(context).colorScheme.inverseSurface, 
+                ),), 
+            
+            const SizedBox(height: 25,), 
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.next,
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      errorBorder: const OutlineInputBorder(
+              //borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), 
+              borderSide: BorderSide(color:  Colors.red), 
+            ), 
+                      enabledBorder: const OutlineInputBorder(
+              //borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), 
+              borderSide: BorderSide(color:  Colors.black), 
+            ), 
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), 
+            ), 
+            hintText: 'Email', 
+            labelText: 'Email',
+         
+            hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),  
+                     // labelText: 'Email'
+                      
+                      ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
                     },
                   ),
                 ),
-                obscureText: !_isPasswordVisible,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              _isLoading
-                  ? CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: () => _login(context),
-                      child: Text('Login'),
+      
+                // Password. 
+      
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                  child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                    textInputAction: TextInputAction.done,
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                       enabledBorder: const OutlineInputBorder(
+            //borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), 
+            borderSide: BorderSide(color:  Colors.black), 
+          ), 
+           errorBorder: const OutlineInputBorder(
+              //borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary), 
+              borderSide: BorderSide(color:  Colors.red), 
+            ), 
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), 
+          ), 
+          hintText: 'Password',
+                     labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isPasswordVisible = !_isPasswordVisible;
+                          });
+                        },
+                      ),
                     ),
-            ],
-          ),
+                    obscureText: !_isPasswordVisible,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+
+                 const SizedBox(height: 10,), 
+
+                 // button. 
+                  _isLoading
+                    ? CircularProgressIndicator()
+                    : AppButton(text: 'Login', 
+                    onTap: () => _login(context),
+                    ), 
+                    // : ElevatedButton(
+                    //     onPressed: () => _login(context),
+                    //     child: Text('Login'),
+                    //   ),
+                      SizedBox(height: 20,), 
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Not a member?' , 
+                    style: TextStyle(
+                      //color: Theme.of(context).colorScheme.inversePrimary
+                      color: Colors.black.withOpacity(0.6), 
+                    ),
+                    ), 
+                    const SizedBox(width: 4,), 
+                    GestureDetector(
+                    //  onTap: widget.onTap, 
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpScreen())),
+                      child: Text('Register here', 
+                      style: TextStyle(
+                        //color: Theme.of(context).colorScheme.inversePrimary, 
+                        color: Colors.black.withOpacity(0.6),
+                        fontWeight: FontWeight.bold, 
+                      ),
+                      ),
+                    ), 
+               
+              ],
+            ),
+         ] ),
         ),
       ),
-    );
+    ));
   }
 }
