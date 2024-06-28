5047b8a6377afd4b1ec5c1f15b868ad8185de7d3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-28 21:40:46 +0100

    Update edit_profile.dart

--- lib/screens/components/profile/edit_profile.dart
+++ lib/screens/components/profile/edit_profile.dart
@@ -8,6 +8,12 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+final _formKey = GlobalKey<FormState>();
+final _firstNameController = TextEditingController(); 
+final _lastNameController = TextEditingController(); 
+final _phoneNumberController = TextEditingController(); 
+final _addressController = TextEditingController(); 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +24,7 @@ class _EditProfileState extends State<EditProfile> {
       body: Center(
         child:  
             Form(
+              key: _formKey, 
               child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -33,16 +40,16 @@ class _EditProfileState extends State<EditProfile> {
                    Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical:10),
                       child: TextFormField(
-                        keyboardType: TextInputType.emailAddress,
+                        keyboardType: TextInputType.name,
                         textInputAction: TextInputAction.next,
-                        //controller: _emailController,
+                        controller: _firstNameController,
                         decoration: InputDecoration(
                              errorBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
                           enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
                             focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), 
               ), 
-              hintText: 'First Name',
+              hintText: 'Change your First Name',
             
                hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary), 
                          labelText: 'First Name'
@@ -50,6 +57,64 @@ class _EditProfileState extends State<EditProfile> {
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return 'Please enter your first name';
+                          }  
+                          return null;
+                        
+                        
+                        },
+                      ),
+                    ),
+
+                         // last Name. 
+                   Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical:10),
+                      child: TextFormField(
+                        keyboardType: TextInputType.name,
+                        textInputAction: TextInputAction.next,
+                        controller: _lastNameController,
+                        decoration: InputDecoration(
+                             errorBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+                          enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), 
+              ), 
+              hintText: 'Change your Last Name',
+            
+               hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary), 
+                         labelText: 'Last Name'
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your last name';
+                          }  
+                          return null;
+                        
+                        
+                        },
+                      ),
+                    ),
+
+                    // address
+                     Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical:10),
+                      child: TextFormField(
+                        keyboardType: TextInputType.name,
+                        textInputAction: TextInputAction.next,
+                        controller: _addressController,
+                        decoration: InputDecoration(
+                             errorBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+                          enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+                            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).colorScheme.primary), 
+              ), 
+              hintText: 'Change your Address',
+            
+               hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary), 
+                         labelText: 'Address'
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your address';
                           }  
                           return null;
                         
