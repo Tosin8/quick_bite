dc0f2e1918558283c03b52929302f379d0863091
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-29 04:39:43 +0100

    Update

---
+++ lib/screens/components/settings/notifications.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class NotificationSettingsScreen extends StatefulWidget {
+  @override
+  _NotificationSettingsScreenState createState() => _NotificationSettingsScreenState();
+}
+
+class _NotificationSettingsScreenState extends State<NotificationSettingsScreen> {
+  bool _orderStatusUpdates = true;
+  bool _promotionsOffers = true;
+  bool _feedbackReviews = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final doc = await FirebaseFirestore.instance.collection('settings').doc(user!.uid).get();
+
+    if (doc.exists) {
+      setState(() {
+        _orderStatusUpdates = doc['orderStatusUpdates'];
+        _promotionsOffers = doc['promotionsOffers'];
+        _feedbackReviews = doc['feedbackReviews'];
+      });
+    }
+  }
+
+  Future<void> _saveSettings() async {
+    final user = FirebaseAuth.instance.currentUser;
+    await FirebaseFirestore.instance.collection('settings').doc(user!.uid).set({
+      'orderStatusUpdates': _orderStatusUpdates,
+      'promotionsOffers': _promotionsOffers,
+      'feedbackReviews': _feedbackReviews,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notification Settings'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          SwitchListTile(
+            title: Text('Order Status Updates'),
+            value: _orderStatusUpdates,
+            onChanged: (bool value) {
+              setState(() {
+                _orderStatusUpdates = value;
+              });
+              _saveSettings();
+            },
+          ),
+          SwitchListTile(
+            title: Text('Promotions and Offers'),
+            value: _promotionsOffers,
+            onChanged: (bool value) {
+              setState(() {
+                _promotionsOffers = value;
+              });
+              _saveSettings();
+            },
+          ),
+          SwitchListTile(
+            title: Text('Feedback and Reviews'),
+            value: _feedbackReviews,
+            onChanged: (bool value) {
+              setState(() {
+                _feedbackReviews = value;
+              });
+              _saveSettings();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,252 +1,74 @@
-
-
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-import 'components/settings/manage_delivery_address.dart';
+import 'components/settings/notifications.dart';
 
-class SettingsScreen extends StatefulWidget {
-  @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
 
-class _SettingsScreenState extends State<SettingsScreen> {
-  bool _darkMode = false;
-  bool _orderStatusUpdates = true;
-  bool _promotionsOffers = true;
-  bool _feedbackReviews = true;
-  bool _biometricAuth = false;
-  bool _twoFactorAuth = false;
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadSettings();
-  }
-
-  Future<void> _loadSettings() async {
-    User? user = _auth.currentUser;
-    if (user != null) {
-      DocumentSnapshot userDoc = await _firestore.collection('users').doc(user.uid).get();
-      setState(() {
-        _darkMode = userDoc['darkMode'] ?? false;
-        _orderStatusUpdates = userDoc['orderStatusUpdates'] ?? true;
-        _promotionsOffers = userDoc['promotionsOffers'] ?? true;
-        _feedbackReviews = userDoc['feedbackReviews'] ?? true;
-        _biometricAuth = userDoc['biometricAuth'] ?? false;
-        _twoFactorAuth = userDoc['twoFactorAuth'] ?? false;
-      });
-    }
-  }
-
-  Future<void> _saveSettings() async {
-    User? user = _auth.currentUser;
-    if (user != null) {
-      await _firestore.collection('users').doc(user.uid).update({
-        'darkMode': _darkMode,
-        'orderStatusUpdates': _orderStatusUpdates,
-        'promotionsOffers': _promotionsOffers,
-        'feedbackReviews': _feedbackReviews,
-        'biometricAuth': _biometricAuth,
-        'twoFactorAuth': _twoFactorAuth,
-      });
-    }
-  }
-
+class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('App Settings'),
+        title: Text('Settings'),
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: [
-          _buildNotificationSettings(),
+          ListTile(
+            title: Text('Notification Settings'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotificationSettingsScreen()),
+              );
+            },
+          ),
           Divider(),
-          _buildPaymentSettings(),
+          ListTile(
+            title: Text('Payment Settings'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PaymentSettingsScreen()),
+              );
+            },
+          ),
           Divider(),
-          _buildPrivacySettings(),
+          ListTile(
+            title: Text('Privacy Settings'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PrivacySettingsScreen()),
+              );
+            },
+          ),
           Divider(),
-          _buildAppPreferences(),
+          ListTile(
+            title: Text('Preferences'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PreferencesScreen()),
+              );
+            },
+          ),
           Divider(),
-          _buildSecuritySettings(),
+          ListTile(
+            title: Text('Security Settings'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecuritySettingsScreen()),
+              );
+            },
+          ),
         ],
       ),
-    );
-  }
-
-  Widget _buildNotificationSettings() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Notification Settings',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SwitchListTile(
-          title: Text('Order Status Updates'),
-          value: _orderStatusUpdates,
-          onChanged: (bool value) {
-            setState(() {
-              _orderStatusUpdates = value;
-            });
-            _saveSettings();
-          },
-        ),
-        SwitchListTile(
-          title: Text('Promotions and Offers'),
-          value: _promotionsOffers,
-          onChanged: (bool value) {
-            setState(() {
-              _promotionsOffers = value;
-            });
-            _saveSettings();
-          },
-        ),
-        SwitchListTile(
-          title: Text('Feedback and Reviews'),
-          value: _feedbackReviews,
-          onChanged: (bool value) {
-            setState(() {
-              _feedbackReviews = value;
-            });
-            _saveSettings();
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildPaymentSettings() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Payment Settings',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        ListTile(
-          title: Text('Manage Saved Delivery Addresses'),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ManageDeliveryAddressesScreen()),
-            );
-          },
-        ),
-        ListTile(
-          title: Text('Set Default Delivery Address'),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SetDefaultDeliveryAddressScreen()),
-            );
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildPrivacySettings() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Privacy Settings',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        ListTile(
-          title: Text('Manage Data Sharing Preferences'),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ManageDataSharingScreen()),
-            );
-          },
-        ),
-        ListTile(
-          title: Text('Opt-in/out of Marketing Communications'),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MarketingCommunicationsScreen()),
-            );
-          },
-        ),
-        ListTile(
-          title: Text('View Privacy Policy'),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => PrivacyPolicyScreen()),
-            );
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildAppPreferences() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'App Preferences',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SwitchListTile(
-          title: Text('Dark Mode'),
-          value: _darkMode,
-          onChanged: (bool value) {
-            setState(() {
-              _darkMode = value;
-            });
-            _saveSettings();
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSecuritySettings() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Security Settings',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SwitchListTile(
-          title: Text('Enable Biometric Authentication'),
-          value: _biometricAuth,
-          onChanged: (bool value) {
-            setState(() {
-              _biometricAuth = value;
-            });
-            _saveSettings();
-          },
-        ),
-        ListTile(
-          title: Text('Set up Two-Factor Authentication'),
-          trailing: Icon(Icons.arrow_forward_ios),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => TwoFactorAuthScreen()),
-            );
-          },
-        ),
-      ],
     );
   }
 }
