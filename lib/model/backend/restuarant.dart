3e0bccaf38e97150e14b29ebc9dcaf2efd7c4b21
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-06 00:57:38 +0100

    Creating backend ui

--- lib/model/backend/restuarant.dart
+++ lib/model/backend/restuarant.dart
@@ -1,18 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:quick_bite/model/backend/food.dart';
+import 'package:quick_bite/model/food.dart';
 
-class Restaurant with ChangeNotifier {
-  List<Food> menu = [];
+class Restaurant extends ChangeNotifier {
+  List<Food> _menu = [];
 
-  void setMenu(List<Food> newMenu) {
-    menu = newMenu;
-    notifyListeners();
+  List<Food> get menu => _menu;
+
+  Restaurant() {
+    fetchMenu();
   }
 
   Future<void> fetchMenu() async {
-    var snapshot = await FirebaseFirestore.instance.collection('foods').get();
-    var foods = snapshot.docs.map((doc) => Food.fromFirestore(doc)).toList();
-    setMenu(foods);
+    final QuerySnapshot result = await FirebaseFirestore.instance.collection('menu').get();
+    final List<DocumentSnapshot> documents = result.docs;
+
+    _menu = documents.map((doc) {
+      final data = doc.data() as Map<String, dynamic>;
+      return Food(
+        name: data['name'],
+        description: data['description'],
+        image: data['image'],
+        price: data['price'],
+        category: FoodCategory.values.firstWhere((e) => e.toString() == data['category']),
+        availableAddons: (data['availableAddons'] as List<dynamic>).map((addon) {
+          return Addon(name: addon['name'], price: addon['price']);
+        }).toList(),
+      );
+    }).toList();
+
+    notifyListeners();
+  }
+
+  void addToCart(Food food, List<Addon> selectedAddons) {
+    // Add food to the cart
+    notifyListeners();
   }
 }
