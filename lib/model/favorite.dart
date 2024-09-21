c4ff13c2f7bf91a28134bd69fc42a41eef74f217
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-21 14:30:19 +0100

    Update favorite.dart

--- lib/model/favorite.dart
+++ lib/model/favorite.dart
@@ -5,3 +5,21 @@ class Product {
 
   Product({required this.id, required this.name, this.isFavorite = false});
 }
+
+
+class FavoriteProvider with ChangeNotifier {
+  final List<Product> _favorites = [];
+
+  List<Product> get favorites => _favorites;
+
+  void toggleFavorite(Product product) {
+    if (_favorites.contains(product)) {
+      _favorites.remove(product);
+      product.isFavorite = false;
+    } else {
+      _favorites.add(product);
+      product.isFavorite = true;
+    }
+    notifyListeners();
+  }
+}
